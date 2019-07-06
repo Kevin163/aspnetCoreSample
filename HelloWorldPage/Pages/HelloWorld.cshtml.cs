1b6c183a8a7f7d14d540207b8ba007e042bb163d
chenql <chenql163@163.com>
2019-07-06 15:28:12 +0800

    增加了form表单提交示例

---
+++ HelloWorldPage/Pages/HelloWorld.cshtml.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelloWorldPage.Pages
+{
+    public class HelloWorld:PageModel
+    {
+        public string Msg => "Hello World from pageModel";
+        public string DateTimeNow => DateTime.Now.ToString();
+        [BindProperty]
+        public string UserName { get; set; }
+        [BindProperty]
+        public string Password { get; set; }
+        public string Result => $"服务器于{DateTime.Now.ToString()}接收到用户{UserName}使用密码{Password}进行登录,登录结果：{LoginResult}";
+        public string LoginResult { get; set; }
+        public Task OnPost()
+        {
+            if(UserName == "superadmin" && Password == "123456")
+            {
+                LoginResult = "登录成功";
+            }
+            else
+            {
+                LoginResult = "密码错误";
+            }
+            return Task.CompletedTask;
+        }
+    }
+}
