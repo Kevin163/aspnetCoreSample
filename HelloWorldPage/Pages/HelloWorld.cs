08860e2c06d0884d163bb5e95f686a051eeb1d42
chenql <chenql163@163.com>
2019-07-06 14:20:47 +0800

    修改了将原来的单页面更改为有后台类型的页面

---
+++ HelloWorldPage/Pages/HelloWorld.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelloWorldPage.Pages
+{
+    public class HelloWorld:PageModel
+    {
+        public string Msg => "Hello World from pageModel";
+        public string DateTimeNow => DateTime.Now.ToString();
+    }
+}
